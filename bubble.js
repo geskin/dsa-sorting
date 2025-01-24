2b3abad823df5ebe7d469835de8d362ea27fcd81
Ehretz Geskin <sg3584@alum.barnard.edu>
2025-01-24 14:09:52 -0500

    Completing basic sorting methods

--- bubble.js
+++ bubble.js
@@ -1,3 +1,15 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+    for (let i = arr.length - 1; i >= 0; i--) {
+        for (let j = 0; j < i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let val1 = arr[j];
+                let val2 = arr[j + 1];
+                arr[j] = val2;
+                arr[j + 1] = val1;
+            }
+        }
+    }
+    return arr;
+}
 
 module.exports = bubbleSort;
--- merge.js
+++ merge.js
@@ -1,5 +1,39 @@
-function merge() {}
+function merge(arr1, arr2) {
+    let out = [];
+    let i = 0;
+    let j = 0;
 
-function mergeSort() {}
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] <= arr2[j]) {
+            out.push(arr1[i]);
+            i++;
+        } else {
+            out.push(arr2[j]);
+            j++;
+        }
+    }
 
-module.exports = { merge, mergeSort};
+    if (i === arr1.length) {
+        for (let k = j; k < arr2.length; k++) {
+            out.push(arr2[k]);
+        }
+    } else if (j === arr2.length) {
+        for (let k = i; k < arr1.length; k++) {
+            out.push(arr1[k]);
+        }
+    }
+
+    return out;
+}
+
+function mergeSort(arr) {
+    if (arr.length < 2) return arr;
+
+    let middleIdx = Math.ceil((arr.length) / 2);
+    let arr1 = arr.slice(0, middleIdx);
+    let arr2 = arr.slice(middleIdx);
+
+    return merge(mergeSort(arr1), mergeSort(arr2));
+}
+
+module.exports = { merge, mergeSort };
